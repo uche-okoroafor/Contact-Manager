aa16d40bfe31152adae49719cbcb69c91f744c5c
Uche Okoroafor <71725892+uche-okoroafor@users.noreply.github.com>
2020-12-17 20:11:33 +0400

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+
+function Counter(props){
+const [arr,setarr] = useState("");
+const [outside,setoutside]=useState('');
+const [load,setload]=useState(props.data);
+
+
+function updatec(e){
+setarr(e.target.value);
+}
+
+
+
+function loadcontact(e){
+setload([...load,arr]);
+setoutside(props.data.map((list,index)=><li key={index}>{list}</li>))
+setarr('');
+e.preventDefault();
+}
+
+
+return <div><form onSubmit={loadcontact}>
+<input type="text" value={arr} onChange={updatec}/>
+<input type="submit" value="Submit"/>
+<p>{outside}{load}</p>
+</form>
+</div>
+}
+
+
+const arr = ["uche okoroafor","chigo okoroafor","chima okoroafor"];
+
+ReactDOM.render( <Counter data={arr}/> 
+,document.getElementById("root")
+)
