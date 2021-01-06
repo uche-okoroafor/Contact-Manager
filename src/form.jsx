afb4cce4b6a73d1fc6d40368645ec3daecb0aa7a
Uche Okoroafor <71725892+uche-okoroafor@users.noreply.github.com>
2021-01-06 21:36:36 +0400

    Update form.jsx

--- src/form.jsx
+++ src/form.jsx
@@ -1,27 +1,60 @@
-import React  from 'react';
-import {connect} from 'react-redux';
+import React, { Component } from "react";
 
- function List(props){
-const contact = props.persons;
-const numbers = props.numbers;
-const address = props.address;
-const listitems = contact.map((list,index)=> <li key={index}>{list}</li> );
-const listnum = numbers.map((list,index)=> <li key={index}>{list}</li> );
-const listaddr = address.map((list,index)=> <li key={index}>{list}</li> );
+class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-return<div> <ul>{listitems}</ul>
-<ul>{listnum}</ul>
-<ul>{listaddr}</ul>
-</div>
- }
+  render() {
+    return (
+      <div style={this.props.displayForm}>
+        <h3>Add Contact Form</h3>
+        <form onSubmit={this.props.handleSubmit}>
+          <input
+            type="text"
+            onChange={this.props.handleNameChange}
+            className="form-control"
+            value={this.props.name}
+            style={this.props.outline}
+            placeholder="Name"
+          />
+          <span style={this.props.display}>Name required</span>
+          <br />
 
-function mapStateToProps(state){
-return{
-persons:state.persons,
-numbers:state.numbers,
-address:state.address
+          <input
+            type="text"
+            onChange={this.props.handleNickNameChange}
+            className="form-control "
+            value={this.props.nickName}
+            placeholder="NickName"
+          />
+          <br />
+          <input
+            type="text"
+            onChange={this.props.handleMobileNumberChange}
+            className="form-control"
+            value={this.props.mobileNumber}
+            style={this.props.outline}
+            placeholder="Mobilenumber"
+          />
+          <span style={this.props.display}>Mobilenumber required</span>
+          <br />
+          <input
+            type="text"
+            onChange={this.props.handleAddressChange}
+            className="form-control"
+            value={this.props.address}
+            placeholder="Address"
+          />
+          <br />
+          <input type="submit" className="btn btn-success  col-md-10 m-2 " value="Add Contact"  style={this.props.displayAddBtn} />
+           <input type="button" className="btn btn-success  col-md-10 m-2 " style={this.props.displayEditBtn} value="Save Contact" onClick={(e)=>this.props.submitEdit(e)} />
+             <input type="button" className="btn btn-warning col-md-10 m-2 " style={this.props.displayEditBtn} value="Cancel Edit" onClick={(e)=>this.props.cancelEdit(e)} />
+        </form>
+      </div>
+    );
+  }
 }
-}
 
-
-export default connect (mapStateToProps)(List);
+export default Form;
