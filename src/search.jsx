aa7c33415ec153158c02394d1a0f8f11993020d4
Uche Okoroafor <71725892+uche-okoroafor@users.noreply.github.com>
2021-01-30 22:05:54 +0400

    Update search.jsx

--- src/search.jsx
+++ src/search.jsx
@@ -1,39 +1,78 @@
 import React, { Component } from "react";
-import {connect} from 'react-redux';
-
-
-
+import { connect } from "react-redux";
+import contacticont from '../images/contacticont.png'
 
 class SearchBar extends Component {
-   constructor(props){
-super(props);
-this.state={
-searchInput:'',
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchInput: "",
+      filteredSearch: [],
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchInput: e.target.value });
+    this.setState({
+      filteredSearch: this.props.contacts.filter((contact) => {
+        return (
+          contact.firstName
+            .toLowerCase()
+            .indexOf(this.state.searchInput.toLowerCase()) !== -1
+        );
+      }),
+    });
+    this.props.toggleDisplay("searchdisplay");
+    e.preventDefault();
+    console.log(this.props.contacts);
+  };
+
+imgSrc=(data)=>{
+if(data[0] === 'h'){
+
+return data;
 }
+return contacticont;
 }
 
 
+  searchReturn(data, index) {
+    return (
+      <div className="search-result-list" style={this.props.displaySearchReturn}>
+        <li
+          key={index}
+          className="list-group-item clearfix  displaylist"
+          onClick={(e) => {
+            this.setState({searchInput:''});
+            this.props.expandContact(index, e);
+            this.props.toggleDisplay("displayList");
+          }}
+        >
+         <img src={this.imgSrc(data.pictureT)} className="thumbnail" alt="icon"/> {data.firstName} {data.lastName}
+        </li>
+      </div>
+    );
+  }
 
-handleChange=(e)=>{
-this.setState({  searchInput:e.target.value   })
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.state.searchInput}
+          onChange={this.handleChange}
+          placeholder="SearchContacts..."
+          className="form-control"
+        />
+        <ul className="mx-auto searchlist">
+          {this.state.filteredSearch.map((contact, i) =>
+            this.searchReturn(contact, i)
+          )}
+        </ul>
+      </div>
+    );
+  }
 }
-
-
-
-    render() { 
-
-
-
- return ( 
-
-<div>
-<input type="text" value={this.state.searchInput} onChange={this.handleChange} placeholder='SearchContacts...'   className="form-control m-5"/>
-</div>
- );
-    }
-}
-
-
 
 const mapStateToProps = (state, ownProps) => {
   return {
@@ -41,7 +80,4 @@ const mapStateToProps = (state, ownProps) => {
   };
 };
 
-
-
- 
-export default  connect(mapStateToProps)(SearchBar);
+export default connect(mapStateToProps, null)(SearchBar);
