ed02a54695046038d56814175df5709b23e24e91
Uche Okoroafor <71725892+uche-okoroafor@users.noreply.github.com>
2021-01-31 21:54:50 +0400

    Create actionTypes.js

---
+++ src/actionTypes.js
@@ -0,0 +1,48 @@
+import * as actionTypes from "./actionTypes";
+
+export const createContact = (
+        title,
+        firstName,
+        lastName,
+        streetName,
+        streetNumber,
+        city,
+        state,
+        country,
+        postCode,
+        email,
+        phone,
+        cell,
+        pictureL,
+        pictureM,
+        pictureT,
+        isChecked,
+       ) => {
+  return {
+    type: actionTypes.CREATE_NEW_CONTACT,
+      title:title,
+        firstName:firstName,
+        lastName:lastName,
+        streetName:streetName,
+        streetNumber:streetNumber,
+        city:city,
+        state:state,
+        country:country,
+        postCode:postCode,
+        email:email,
+        phone:phone,
+        cell:cell,
+        pictureL:pictureL,
+        pictureM:pictureM,
+        pictureT:pictureT,
+        isChecked:isChecked,
+        
+  };
+};
+
+export const deleteContact = (id) => {
+  return {
+    type: actionTypes.REMOVE_CONTACT,
+    id: id,
+  };
+};
