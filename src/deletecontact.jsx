bcdafadcda82250e7cc87e87102904af0b56d65a
Uche Okoroafor <71725892+uche-okoroafor@users.noreply.github.com>
2021-02-01 22:01:21 +0400

    Update deletecontact.jsx

--- src/deletecontact.jsx
+++ src/deletecontact.jsx
@@ -1,87 +1,167 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import * as contactAction from "../actions/contactAction";
-import contacticont from '../images/contacticont.png'
-
-
-
+import contacticont from "../images/contacticont.png";
 
 class DeleteContacts extends Component {
-constructor(props){
-super(props);
+  constructor(props) {
+    super(props);
 
-this.state={}
-}
-   
-
- deleteContact=(e, index)=> {
-    e.preventDefault();
-   this.props.deleteContact(index);
-    
+    this.state = {
+      // checkbox: [
+      //   false,
+      //   false,
+      //   false,
+      //   false,
+      //   false,
+      //   false,
+      //   false,
+      //   false,
+      //   false,
+      //   false,
+      //   false,
+      //   false,
+      //   false,
+      //   false,
+      //   false,
+      //   false,
+      //   false,
+      //   false,
+      //   false,
+      //   false,
+      //   false,
+      //   false,
+      //   false,
+      //   false,
+      //   false,
+      //   false,
+      //   false,
+      //   false,
+      //   false,
+      //   false,
+      //   false,
+      //   false,
+      //   false,
+      // ],
+    checkbox:this.props.checkbox,
+     
+    };
   }
 
- imgSrc=()=>{
-if(this.props.contacts.pictureL[0] !== 'h'){
+  createArray = () => {
+    this.setState({
+  checkbox:this.props.contacts.map((contacts)=>contacts.isChecked)
+    });
+console.log(this.state.checkbox);
+  };
 
-return contacticont;
-}
-return this.props.contacts.pictureL;
-}
+  //  e.preventDefault();
 
+  // let checkbox = { checkbox: this.state.checkbox };
+  // this.props.createContact()
 
+  // console.log(this.state.checkbox)
+  // }
 
-
-
-
-allCountacts=()=>{
-
-const list = this.props.contacts.map((contact, index)=>(
-
-     <li
-          key={index}
-        
-          className={'list-group-item clearfix displaylist'}
-
-        > <img src={this.imgSrc()}alt="icon"/>  {contact.firstName} {contact.lastName}
- <button
-        onClick={(e) =>this.deleteContact(e,index)}
-        className="btn btn-danger form-control  deletebutton"
-      >
-        Delete
-      </button>
-   
-<input type="checkbox" />
-        </li>
+  handlechange = (index, e) => {
+    let arr = this.state.checkbox;
+    arr[index] = e.target.checked;
+    this.setState({
+      checkbox: arr,
+    });
  
+  };
+
+  // handleChange=(e)=>{
+
+  // // if(e.target.value === 'false'){ e.target.value ='true'}
+
+  // // if(e.target.value === 'true'){
+
+  // // e.target.value='false';
+  // // }
+
+  // e.target.value ='true'
+
+  // this.setState({
+  // checkbox:this.state.checkbox+1
+  // });
+  // }
+
+  handledelete = (e) => {
+let checkedbox = document.getElementsByClassName("checkbox");  
+    for (let i = 0; i < checkedbox.length; i++) {
+      if (checkedbox[i].checked === true ) {
+        this.deleteContact(e, i);
+let arr = this.state.checkbox;
+arr.splice(i, 1);
+this.setState({
+checkbox:arr
+})
+
+      }
+    }
+console.log(this.state.checkbox,checkedbox.length,this.state.checkbox.length)
+  };
 
 
- ))
-console.log(this.props.contacts)
-    
-return list;
 
+  deleteContact = (e, index) => {
+    e.preventDefault();
+    this.props.deleteContact(index);
+this.createArray()
+  };
+
+  imgSrc = (contact) => {
+    if (contact.pictureL[0] === "h") {
+      return contact.pictureL;
+    }
+
+    return contacticont;
+  };
+
+  allCountacts = () => {
+    const list = this.props.contacts.map((contact, index) => (
+      <li key={index} className={"list-group-item clearfix displaylist"}>
+        {" "}
+        <img
+          src={this.imgSrc(contact)}
+          alt="icon"
+          className="thumbnail ml-2"
+        />{" "}
+        {contact.firstName} {contact.lastName}
+        <button
+          onClick={(e) => this.deleteContact(e, index)}
+          className="btn btn-danger form-control  deletebutton"
+        >
+          Delete
+        </button>
+        <input
+          type="checkbox"
+          class="checkbox"
+          name="checkbox"
+          checked={this.state.checkbox[index]}
+          //   onClick={this.handleChange}
+          onChange={(e) => this.handlechange(index, e)}
+        />
+      </li>
+    ));
+
+    return list;
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <ul>{this.allCountacts()}</ul>
+
+        <button className="btn bg-danger" onClick={this.handledelete}>
+          btndele
+        </button>
+      </React.Fragment>
+    );
+  }
 }
-
-
-
-
-
-    render() { 
-        return ( 
-<React.Fragment>
-<ul>
-{this.allCountacts()
-}
-</ul>
-</React.Fragment>
- );
-    
-}
- 
-}
-
-
-
 
 const mapStateToProps = (state, ownProps) => {
   return {
@@ -91,41 +171,47 @@ const mapStateToProps = (state, ownProps) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    createContact: (    title,
-    firstName,
-    lastName,
-    streetNum,
-    streetName,
-    city,
-    state,
-    country,
-    postcode,
-    email,
-    phone,
-    cell,
-    pictureL,
-    pictureM,
-    pictureT) =>
+    createContact: (
+      title,
+      firstName,
+      lastName,
+      streetNum,
+      streetName,
+      city,
+      state,
+      country,
+      postcode,
+      email,
+      phone,
+      cell,
+      pictureL,
+      pictureM,
+      pictureT,
+      isChecked
+    ) =>
       dispatch(
-        contactAction.createContact(title,
-    firstName,
-    lastName,
-    streetNum,
-    streetName,
-    city,
-    state,
-    country,
-    postcode,
-    email,
-    phone,
-    cell,
-    pictureL,
-    pictureM,
-    pictureT)
+        contactAction.createContact(
+          title,
+          firstName,
+          lastName,
+          streetNum,
+          streetName,
+          city,
+          state,
+          country,
+          postcode,
+          email,
+          phone,
+          cell,
+          pictureL,
+          pictureM,
+          pictureT,
+          isChecked
+        )
       ),
 
-    deleteContact:(index) => dispatch(contactAction.deleteContact(index)),
+    deleteContact: (index) => dispatch(contactAction.deleteContact(index)),
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)( DeleteContacts);
+export default connect(mapStateToProps, mapDispatchToProps)(DeleteContacts);
