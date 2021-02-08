d08bf3f9b95a13d6193635033f629cbc07c3000a
Uche Okoroafor <71725892+uche-okoroafor@users.noreply.github.com>
2021-02-08 22:25:28 +0400

    Update deletecontact.jsx

--- src/deletecontact.jsx
+++ src/deletecontact.jsx
@@ -8,60 +8,35 @@ class DeleteContacts extends Component {
     super(props);
 
     this.state = {
-      // checkbox: [
-      //   false,
-      //   false,
-      //   false,
-      //   false,
-      //   false,
-      //   false,
-      //   false,
-      //   false,
-      //   false,
-      //   false,
-      //   false,
-      //   false,
-      //   false,
-      //   false,
-      //   false,
-      //   false,
-      //   false,
-      //   false,
-      //   false,
-      //   false,
-      //   false,
-      //   false,
-      //   false,
-      //   false,
-      //   false,
-      //   false,
-      //   false,
-      //   false,
-      //   false,
-      //   false,
-      //   false,
-      //   false,
-      //   false,
-      // ],
-    checkbox:this.props.checkbox,
-     
+      checkbox: this.props.checkbox,
     };
+
   }
 
-  createArray = () => {
-    this.setState({
-  checkbox:this.props.contacts.map((contacts)=>contacts.isChecked)
-    });
-console.log(this.state.checkbox);
-  };
+//   componentDidMount() {
+//     this.createArrayFalse()
+//   }
 
-  //  e.preventDefault();
+//   createArrayFalse = () => {
+//     let arr = this.state.checkbox;
+//     for (let i = 0; i < 34; i++) {
+//       arr = [...arr, false];
+//     }
+//     this.setState({
+//       checkbox: arr,
+//     });
+//   };
 
-  // let checkbox = { checkbox: this.state.checkbox };
-  // this.props.createContact()
+//  createArrayTrue = () => {
+//     let arr = this.state.checkbox;
+//     for (let i = 0; i < 34; i++) {
+//       arr = [...arr, true];
+//     }
+//     this.setState({
+//       checkbox: arr,
+//     });
+//   };
 
-  // console.log(this.state.checkbox)
-  // }
 
   handlechange = (index, e) => {
     let arr = this.state.checkbox;
@@ -69,47 +44,50 @@ console.log(this.state.checkbox);
     this.setState({
       checkbox: arr,
     });
- 
   };
 
-  // handleChange=(e)=>{
 
-  // // if(e.target.value === 'false'){ e.target.value ='true'}
+handleAllDelete = (e) => {
 
-  // // if(e.target.value === 'true'){
-
-  // // e.target.value='false';
-  // // }
-
-  // e.target.value ='true'
-
-  // this.setState({
-  // checkbox:this.state.checkbox+1
-  // });
-  // }
-
-  handledelete = (e) => {
-let checkedbox = document.getElementsByClassName("checkbox");  
-    for (let i = 0; i < checkedbox.length; i++) {
-      if (checkedbox[i].checked === true ) {
+    for (let i = 0; i < this.state.checkbox.length; i++) {
+      if (this.state.checkbox[i] === true) {
         this.deleteContact(e, i);
-let arr = this.state.checkbox;
-arr.splice(i, 1);
-this.setState({
-checkbox:arr
-})
-
+        let arr = this.state.checkbox;
+        arr.splice(i, 1);
+        this.setState({
+          checkbox: arr,
+        });
       }
     }
-console.log(this.state.checkbox,checkedbox.length,this.state.checkbox.length)
+
   };
+
+
+runFunction=(e,activate)=>{
+
+if(activate==='DeleteSelectedContacts')
+{
+
+this.handleAllDelete(e);
+}
+else if(activate==='SelectAllContacts'){
+this.createArrayTrue();
+}
+}
+
+
+
+
+
+
+
 
 
 
   deleteContact = (e, index) => {
     e.preventDefault();
     this.props.deleteContact(index);
-this.createArray()
+    this.props.createArrayFalse();
   };
 
   imgSrc = (contact) => {
@@ -141,7 +119,6 @@ this.createArray()
           class="checkbox"
           name="checkbox"
           checked={this.state.checkbox[index]}
-          //   onClick={this.handleChange}
           onChange={(e) => this.handlechange(index, e)}
         />
       </li>
@@ -151,13 +128,17 @@ this.createArray()
   };
 
   render() {
+
+
+
     return (
       <React.Fragment>
         <ul>{this.allCountacts()}</ul>
 
-        <button className="btn bg-danger" onClick={this.handledelete}>
+        <button className="btn bg-danger" onClick={this.handleAllDelete}>
           btndele
         </button>
+<input type="text" value={(e)=>this.runFunction(e,this.props.activate)} style={{display:'none'}}/>
       </React.Fragment>
     );
   }
