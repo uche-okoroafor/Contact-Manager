92176b3e289ed9c588bbecf64d567c4da801d9fa
Uche Okoroafor <71725892+uche-okoroafor@users.noreply.github.com>
2021-02-10 21:49:16 +0400

    Update form.jsx

--- src/form.jsx
+++ src/form.jsx
@@ -147,7 +147,69 @@ class Form extends Component {
                     class="form-control form-control-alternative"
                     value={this.props.streetName}
                     placeholder="Street Name"
-                  />
+                  />import React  from "react";
+
+function SideButtons(props) {
+
+
+
+
+
+
+  return (
+    <React.Fragment>
+      <button 
+        className="btn bg-success  m-2 sidebutton"
+        onClick={() => props.toggleDisplay("displayCreateNewContact")}
+      >
+        Create New Contact
+      </button>
+
+
+ <button
+        className="btn btn-info  m-2 sidebutton"
+        onClick={() => props.toggleDisplay("displayAll4edit")}
+      >
+     Edit Contacts
+      </button>
+
+
+ <button
+        className="btn bg-danger  m-2 sidebutton"
+        onClick={() =>{ props.toggleDisplay("displayDeleteContacts")
+}                
+}
+       style={props.displayDeleteBtn1}
+      >
+       Delete Contacts
+      </button>
+
+
+ <button
+        className="btn bg-danger  m-2 sidebutton"
+        onClick={() =>{props.createArrayTrue();
+        }}
+       style={props.displayDeleteBtn2}
+      >
+      Select All Contacts
+      </button>
+
+ <button
+        className="btn bg-danger  m-2 sidebutton"
+     onClick={() =>{props.createArrayTrue();
+        }}
+       style={props.displayDeleteBtn2}
+      >
+       Delete Selected Contacts
+      </button>
+
+
+    </React.Fragment>
+  );
+}
+
+export default SideButtons;
+
                 </div>
               </div>
               <div class="col-lg-6">
