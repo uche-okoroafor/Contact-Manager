d5dbb7c7a8b1f0bedeaa1e7114fb7662459ee712
Uche Okoroafor <71725892+uche-okoroafor@users.noreply.github.com>
2021-02-15 22:32:42 +0400

    Update deleteContacts.jsx

--- src/components/deleteContacts.jsx
+++ src/components/deleteContacts.jsx
@@ -27,49 +27,13 @@ this.props.reduceCheckboxArray(index);
     return contacticont;
   };
 
-  allCountacts = () => {
-    const list = this.props.contacts.map((contact, index) => (
-      <li key={index} className={"list-group-item clearfix displaylist"}>
-        {" "}
-        <img
-          src={this.imgSrc(contact)}
-          alt="icon"
-          className="thumbnail ml-2"
-        />{" "}
-        {contact.firstName} {contact.lastName}
-        <button
-          onClick={(e) => this.deleteContact(e, index)}
-          className="btn btn-danger form-control  deletebutton"
-        >
-          Delete
-        </button>import React, { Component } from "react";
-import contacticont from "../images/contacticont.png";
 
+ischecked=(index)=>{
+const filtered = this.props.checkbox.filter((contacts)=>contacts.id === index );
+console.log(index,filtered[0].checkbox);
+return filtered[0].checkbox
+}
 
-
-
-class DeleteContacts extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-   
-    };
-
-  }
-  deleteContact = (e, index) => {
-    e.preventDefault();
-    this.props.deleteContact(e,index);
-this.props.reduceCheckboxArray(index);
-  };
-
-  imgSrc = (contact) => {
-    if (contact.pictureL[0] === "h") {
-      return contact.pictureL;
-    }
-
-    return contacticont;
-  };
 
   allCountacts = () => {
 this.props.contacts.sort(function (a, b) {
@@ -84,8 +48,8 @@ this.props.contacts.sort(function (a, b) {
       return 0;
     });
 
-   const mappedList = this.props.contacts.map((contact, index) => (
-      <li key={index} className={"list-group-item clearfix displaylist"}>
+   const mappedList = this.props.contacts.map((contact) => (
+      <li key={contact.id} className={"list-group-item clearfix displaylist"}>
         {" "}
         <img
           src={this.imgSrc(contact)}
@@ -94,7 +58,7 @@ this.props.contacts.sort(function (a, b) {
         />{" "}
         {contact.firstName} {contact.lastName}
         <button
-          onClick={(e) => this.deleteContact(e, index)}
+          onClick={(e) => this.deleteContact(e, contact.id)}
           className="btn btn-danger form-control  deletebutton"
         >
           Delete
@@ -103,9 +67,10 @@ this.props.contacts.sort(function (a, b) {
           type="checkbox"
           class="checkbox"
           name="checkbox"
-          checked={this.props.checkbox[index]}
-          onChange={(e) => this.props.handleCheckboxChange(index, e)}
+          checked={this.ischecked(contact.id)}
+          onChange={(e) => this.props.handleCheckboxChange(contact.id, e)}
         />
+
       </li>
 
     ));
@@ -115,36 +80,7 @@ this.props.contacts.sort(function (a, b) {
   };
 
   render() {
-console.log(this.props.checkbox)
-    return (
-      <React.Fragment>
-
-        <ul>{this.allCountacts()}</ul>
-      </React.Fragment>
-    );
-  }
-}
-
-
-export default DeleteContacts;
-
-        <input
-          type="checkbox"
-          class="checkbox"
-          name="checkbox"
-          checked={this.props.checkbox[index]}
-          onChange={(e) => this.props.handleCheckboxChange(index, e)}
-        />
-      </li>
-
-    ));
-
-
-    return list;
-  };
-
-  render() {
-console.log(this.props.checkbox)
+// console.log(document.getElementsByTagName('li'))
     return (
       <React.Fragment>
 
