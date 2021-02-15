c6726d2911aea2b32276a36018904ee7867c86a1
Uche Okoroafor <71725892+uche-okoroafor@users.noreply.github.com>
2021-02-15 22:33:24 +0400

    Update recentlyadded.jsx

--- src/components/recentlyadded.jsx
+++ src/components/recentlyadded.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import contacticont from "../images/contacticont.png";
 
 
@@ -18,11 +18,60 @@ function RecentlyAdded(props) {
   const imgframe = 'recentdiv';
   const disEmptyDiv ='recentdivi';
   const thumbnail ="thumbnail";
+const [sortDisplay1,setSortDisplay1] = useState( {display:'block'} )
+const [sortDisplay2,setSortDisplay2] = useState( {display:'none'} )
+const [toggle,setToggle]=useState(true);
+const[imgUrl,setImgUrl]=useState("https://image.flaticon.com/icons/png/512/31/31283.png")
+
+
+
+
+const sortContacts=()=>{
+if(toggle)
+{
+setSortDisplay1({display:'none'});
+setToggle(!toggle);
+setImgUrl("https://image.flaticon.com/icons/png/512/31/31232.png")
+return setSortDisplay2({display:'block'});
+}
+
+setToggle(!toggle);
+setSortDisplay1({display:'block'});
+setImgUrl("https://image.flaticon.com/icons/png/512/31/31283.png")
+return setSortDisplay2(
+{display:'none'}
+);
+}
 
 
 
 
   function recentContact() {
+    const list = props.contacts.map((contact) => (
+      <li
+        key={contact.id}
+        style={activeClick(contact.id, props.id)}
+        className={"list-group-item clearfix displaylist"}
+        onClick={(e) => {
+          props.expandContact(contact.id, e);
+        }}
+      >
+        <img
+          src={imgSrc(contact)}
+          className={activeThumbnail(contact.id, props.id)}
+          alt="contacticont"
+        />
+        <div className={displayDiv(contact.id, props.id)} ></div>{" "}
+        {contact.firstName} {contact.lastName}
+      </li>
+    ));
+    return list;
+  }
+
+
+
+
+  function recentContact2() {
     const list = props.contacts.map((contact, index) => (
       <li
         key={index}
@@ -75,8 +124,13 @@ function RecentlyAdded(props) {
 
   return (
     <React.Fragment>
+  <div className="recent-contact-heading">
+            <div></div>    <h5 className="text-center p-2">Recently Added Contacts</h5>
+<img src={imgUrl}  onClick={sortContacts}   alt='sorticon'  />
+              </div>
       <div className="recentlyadded">
-        <ul>{recentContact()}</ul>
+        <ul style={sortDisplay1}>{recentContact()}</ul>
+ <ul style={sortDisplay2}>{recentContact2()}</ul>
       </div>
     </React.Fragment>
   );
