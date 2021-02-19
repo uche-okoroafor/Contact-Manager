96d2cf06b1b6811e7b4b6696a3d8b52b8ac44ccc
Uche Okoroafor <71725892+uche-okoroafor@users.noreply.github.com>
2021-02-19 07:15:59 +0400

    Update deleteContacts.jsx

--- src/components/deleteContacts.jsx
+++ src/components/deleteContacts.jsx
@@ -1,94 +1,81 @@
-import React, { Component } from "react";
-import contacticont from "../images/contacticont.png";
+import React, { useEffect } from 'react';
+import contacticont from '../images/contacticont.png';
 
+function DeleteContacts(props) {
+	useEffect(
+		() => {
+			props.createArray();
+			props.displayContactEmpty();
+		},
+		[ props.contacts ]
+	);
 
+	const isChecked = (index) => {
+		let value = null;
+		const filtered = props.checkbox.filter((contacts) => contacts.id === index);
+		value = filtered[0].checkbox;
+		return value;
+	};
 
+	const deleteContact = (e, index) => {
+		e.preventDefault();
+		props.deleteContact(e, index);
+		props.createArray();
+	};
 
-class DeleteContacts extends Component {
-  constructor(props) {
-    super(props);
+	const imgSrc = (contact) => {
+		if (contact.pictureL[0] === 'h') {
+			return contact.pictureL;
+		}
 
-    this.state = {
-   
-    };
+		return contacticont;
+	};
 
-  }
-  deleteContact = (e, index) => {
-    e.preventDefault();
-    this.props.deleteContact(e,index);
-this.props.reduceCheckboxArray(index);
-  };
+	const allCountacts = () => {
+		props.checkbox.sort(function(a, b) {
+			var nameA = a.firstName.toUpperCase();
+			var nameB = b.firstName.toUpperCase();
+			if (nameA < nameB) {
+				return -1;
+			}
+			if (nameA > nameB) {
+				return 1;
+			}
+			return 0;
+		});
 
-  imgSrc = (contact) => {
-    if (contact.pictureL[0] === "h") {
-      return contact.pictureL;
-    }
+		const mappedList = props.checkbox.map((contact) => (
+			<li key={contact.id} className={'list-group-item clearfix displaylist checking'}>
+				{' '}
+				<img src={imgSrc(contact)} alt="icon" className="thumbnail ml-2" /> {contact.firstName}{' '}
+				{contact.lastName}
+				<button
+					onClick={(e) => deleteContact(e, contact.id)}
+					className="btn btn-danger form-control  deletebutton"
+				>
+					Delete
+				</button>
+				<input
+					type="checkbox"
+					className="checkbox"
+					name="checkbox"
+					value={console.log(isChecked(contact.id), 'idddd')}
+					checked={isChecked(contact.id)}
+					onChange={(e) => props.handleCheckboxChange(contact.id, e)}
+				/>
+			</li>
+		));
 
-    return contacticont;
-  };
+		return mappedList;
+	};
 
-
-ischecked=(index)=>{
-const filtered = this.props.checkbox.filter((contacts)=>contacts.id === index );
-console.log(index,filtered[0].checkbox);
-return filtered[0].checkbox
+	return (
+		<React.Fragment>
+			{console.log(props.checkbox, 'props')}
+			{/* {console.log(props.contacts.length,'contacts')} */}
+			<ul>{allCountacts()}</ul>
+		</React.Fragment>
+	);
 }
-
-
-  allCountacts = () => {
-this.props.contacts.sort(function (a, b) {
-      var nameA = a.firstName.toUpperCase();
-      var nameB = b.firstName.toUpperCase();
-      if (nameA < nameB) {
-        return -1;
-      }
-      if (nameA > nameB) {
-        return 1;
-      }
-      return 0;
-    });
-
-   const mappedList = this.props.contacts.map((contact) => (
-      <li key={contact.id} className={"list-group-item clearfix displaylist"}>
-        {" "}
-        <img
-          src={this.imgSrc(contact)}
-          alt="icon"
-          className="thumbnail ml-2"
-        />{" "}
-        {contact.firstName} {contact.lastName}
-        <button
-          onClick={(e) => this.deleteContact(e, contact.id)}
-          className="btn btn-danger form-control  deletebutton"
-        >
-          Delete
-        </button>
-        <input
-          type="checkbox"
-          class="checkbox"
-          name="checkbox"
-          checked={this.ischecked(contact.id)}
-          onChange={(e) => this.props.handleCheckboxChange(contact.id, e)}
-        />
-
-      </li>
-
-    ));
-
-
-    return mappedList
-  };
-
-  render() {
-// console.log(document.getElementsByTagName('li'))
-    return (
-      <React.Fragment>
-
-        <ul>{this.allCountacts()}</ul>
-      </React.Fragment>
-    );
-  }
-}
-
 
 export default DeleteContacts;
